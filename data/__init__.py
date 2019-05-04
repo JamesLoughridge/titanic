a080c1eb0ff70fdc97e52a763d38c44f1bbe5771
James Loughridge <jackelthesnake@pm.me>
2019-05-04 10:14:26 -0700

    Replaced conda environment with anaconda-project.

--- data/__init__.py
+++ data/__init__.py
@@ -1,11 +1,5 @@
 from pandas import DataFrame
 
 
-def find_missing_values(df: DataFrame):
-    return {x : df.loc[:, x].isna().sum() for x in df.columns.tolist()}
-
-
 def find_title(df: DataFrame):
     return df.loc[:, 'Name'].str.extract(' (\w+\.)', expand=False)
-
-
